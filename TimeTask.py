e4c22ddac88d5a8865c35643b88ca38b64a478e6
Szymon Jaroszyński <szymjare@gmail.com>
2022-10-28 18:04:05 +0200

    global is probably bad way to do this, and ofc gui seems to need some command in order to refresh

--- TimeTask.py
+++ TimeTask.py
@@ -3,22 +3,26 @@ from datetime import datetime
 import tkinter as tk
 
 startTime = 0
-endTime = 0
+endTime = 5
 taskOngoing = 0
 
-class timeManagement():
-    def __init__(self):
-        self.startTime = 0
-        self.endTime = 0
-        self.taskOngoing = 0
-    def setStartTime(self):
-        self.startTime = (datetime.now())
-        startTimeLabel=tk.Label(text=startTime, bg="white")
-        startTimeLabel.place(x=25,y=85)
-    def setEndTime(self):
-       self.endTime = (datetime.now())
-       endTimeLabel=tk.Label(text=endTime, bg="white")
-       endTimeLabel.place(x=200,y=190)
+def setStartTime():
+    global startTime
+    startTime = datetime.now()
+    global taskOngoing
+    taskOngoing = 1
+    startTimeLabel=tk.Label(text=startTime, bg="white")
+    startTimeLabel.place(x=25,y=85)
+    print(taskOngoing)
+
+def setEndTime():
+    global endTime
+    endTime = datetime.now()
+    global taskOngoing
+    taskOngoing = 0
+    endTimeLabel=tk.Label(text=endTime, bg="white")
+    endTimeLabel.place(x=200,y=85)
+
 ## GUI
 root = tk.Tk()
 root.title("TimeTask")
@@ -26,10 +30,15 @@ root.resizable(width=True, height=True)
 
 canvas = tk.Canvas(root, height=500, width=1000, bg ="#404040").pack()
 if taskOngoing == 0:
-    buttonStart=tk.Button(canvas, text="Start task", command=timeManagement.setStartTime)
+    buttonStart=tk.Button(canvas, text="Start task", command=setStartTime )
     buttonStart.place(x=100,y=100)
-if taskOngoing == 1:
-    buttonEnd=tk.Button(canvas, text="End task", command=timeManagement.setEndTime)
-    buttonEnd.place(x=100,y=200)
+else:
+    buttonEnd=tk.Button(canvas, text="End task", command=setEndTime )
+    buttonEnd.place(x=300,y=100)
+
+
+
+
+
 
 root.mainloop()
