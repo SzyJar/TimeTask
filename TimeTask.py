bfe5256bb0204d4ed88581623856dd7e7def334b
Szymon Jaroszyński <szymjare@gmail.com>
2022-10-28 19:10:46 +0200

    I can count time and save it to file

--- TimeTask.py
+++ TimeTask.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-
 import tkinter as tk
+import csv
+import sys
 
 startTime = 0
-endTime = 5
+endTime = 0
 taskOngoing = 0
 
 def setStartTime():
@@ -11,34 +12,39 @@ def setStartTime():
     startTime = datetime.now()
     global taskOngoing
     taskOngoing = 1
-    startTimeLabel=tk.Label(text=startTime, bg="white")
-    startTimeLabel.place(x=25,y=85)
-    print(taskOngoing)
 
 def setEndTime():
     global endTime
     endTime = datetime.now()
     global taskOngoing
     taskOngoing = 0
-    endTimeLabel=tk.Label(text=endTime, bg="white")
-    endTimeLabel.place(x=200,y=85)
+    global taskTime 
+    taskTime = endTime - startTime
+    print(taskTime)
+    with open('savedTasks.csv', 'a', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow([taskTime])
 
+## Open data file if possible, if not then make data file
+try:
+    with open('savedTasks.csv', newline='') as file:
+        reader = csv.reader(file, delimiter=";")
+        for row in reader:
+            print(', '.join(row))
+except:
+    with open('savedTasks.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
 ## GUI
 root = tk.Tk()
 root.title("TimeTask")
-root.resizable(width=True, height=True)
+root.resizable(width=False, height=False)
 
-canvas = tk.Canvas(root, height=500, width=1000, bg ="#404040").pack()
-if taskOngoing == 0:
-    buttonStart=tk.Button(canvas, text="Start task", command=setStartTime )
-    buttonStart.place(x=100,y=100)
-else:
-    buttonEnd=tk.Button(canvas, text="End task", command=setEndTime )
-    buttonEnd.place(x=300,y=100)
+canvas = tk.Canvas(root, height=500, width=500, bg ="#404040").pack()
 
+buttonStart=tk.Button(canvas, text="Start task", command=setStartTime)
+buttonStart.place(x=50,y=400, height = 50, width = 150)
 
-
-
-
+buttonEnd=tk.Button(canvas, text="End task", command=setEndTime)
+buttonEnd.place(x=300,y=400, height = 50, width = 150)
 
 root.mainloop()
