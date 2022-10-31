d2296aa75952c2e30183f0438840945e5b87d31c
RedGlimmer <107247457+RedGlimmer@users.noreply.github.com>
2022-10-31 11:42:19 +0100

    now status is shown

--- TimeTask.py
+++ TimeTask.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import StringVar, messagebox
 import csv as csv
-import sys
 
 startTime = 0
 endTime = 0
@@ -13,8 +12,6 @@ taskName = ''
 try:
     with open('savedTasks.csv', newline='') as file:
         reader = csv.reader(file, delimiter=";")
-        for row in reader:
-            print(', '.join(row))
 except:
     with open('savedTasks.csv', 'w', newline='') as file:
         writer = csv.writer(file)
@@ -24,6 +21,10 @@ except:
 def setTaskName():
     global taskName
     taskName = entryTaskName.get()
+    if taskName !='':
+        statusInfo.set('Currnet task name: '+ taskName)
+    else:
+        statusInfo.set('Waiting for task name...')
 
 ## Get start time
 def setStartTime():
@@ -32,7 +33,7 @@ def setStartTime():
         startTime = datetime.now()
         global taskOngoing
         taskOngoing = 1
-        root.title("TimeTask (Current task name: "+taskName+")")
+        root.title("TimeTask (Current task: "+taskName+")")
     else:
         messagebox.showwarning("No task name", "Please set task name before starting task time counter.")
 
@@ -59,13 +60,13 @@ root.resizable(width=False, height=False)
 canvas = tk.Canvas(root, height=500, width=500, bg ="#404040").pack()
 
 ## Buttons
-buttonStart=tk.Button(canvas, text="Start task", command=setStartTime)
+buttonStart=tk.Button(canvas, text="Start task", command=setStartTime, font=("Tahoma 9 bold"))
 buttonStart.place(x=50,y=430, height = 50, width = 100)
 
-buttonEnd=tk.Button(canvas, text="Set task name", command=setTaskName)
+buttonEnd=tk.Button(canvas, text="Set task name", command=setTaskName, font=("Tahoma 9 bold"))
 buttonEnd.place(x=200,y=430, height = 50, width = 100)
 
-buttonEnd=tk.Button(canvas, text="End task", command=setEndTime)
+buttonEnd=tk.Button(canvas, text="End task", command=setEndTime, font=("Tahoma 9 bold"))
 buttonEnd.place(x=350,y=430, height = 50, width = 100)
 
 ## Entry box
@@ -74,4 +75,16 @@ entryTaskName.place(x=250, y=380, height = 30, width = 150)
 
 labelTaskName=tk.Label(canvas, text="Enter task name:", bg="#404040", fg="white", font=("Tahoma", 12), justify="left")
 labelTaskName.place(x=100, y=380, height = 30, width = 150)
+
+## show status
+statusInfo=StringVar()
+statusInfo.set('Waiting for task name...')
+labelStatus=tk.Label(canvas, textvariable=statusInfo, bg="#404040", fg="white", font=("Tahoma", 12), justify="center")
+labelStatus.place(x=150, y=340, height = 30, width = 200)
+
+##show data from file
+frameTasks=tk.Frame(canvas, height=320, width=483, bg ="#595959")
+frameTasks.place(x=10, y=10)
+
+
 root.mainloop()
